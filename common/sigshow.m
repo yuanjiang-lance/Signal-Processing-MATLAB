db9f73bb0b2e36587e967f50cff31b10bb3d57c9
yuanjiang-lance <lance682@qq.com>
2023-08-31 18:34:00 -0500

    update: modified figure and font sizes

--- common/sigshow.m
+++ common/sigshow.m
@@ -1,4 +1,4 @@
-function sigshow(Sig,Fs)
+function sigshow(Sig, Fs)
 %
 % Showing one-dimentional signal
 %
@@ -13,11 +13,11 @@ N = length(Sig);
 t = (0: N-1)/Fs;
 
 figure
-set(gcf,'Position',[20 100 640 500]);	    
+set(gcf,'Position',[20 100 415 200]);	    
 set(gcf,'Color','w'); 
-plot(t,Sig,'b','linewidth',2);
-xlabel('Time (s)','FontSize',24,'FontName','Times New Roman');
-ylabel('Amplitude (AU)','FontSize',24,'FontName','Times New Roman');
-set(gca,'FontSize',24)
-set(gca,'linewidth',2);
+plot(t,Sig,'b','linewidth',0.5);
+xlabel('Time (s)','FontSize',12,'FontName','Times New Roman');
+ylabel('Amplitude (AU)','FontSize',12,'FontName','Times New Roman');
+set(gca,'FontSize',12)
+set(gca,'linewidth',1);
 set(gca,'FontName','Times New Roman')
