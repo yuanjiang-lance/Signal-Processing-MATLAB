3d1cac08d1ae493e14e5593cae08e5f227afb18b
yuanjiang-lance <lance682@qq.com>
2023-08-31 23:57:13 -0500

    update: calculating form

--- common/Differ.m
+++ common/Differ.m
@@ -13,8 +13,5 @@ function ybar = Differ(y,delta)
 % Time: 2023-07-30
 
 L = length(y);
-ybar = zeros(1, L-2);
-for i = 2: L-1
-  ybar(i-1) = (y(i+1)-y(i-1)) / (2*delta);
-end
+ybar = (y(3:end) - y(1:end-2)) / (2*delta);
 ybar = [(y(2)-y(1))/delta, ybar, (y(end)-y(end-1))/delta];
