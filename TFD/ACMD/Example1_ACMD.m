885a539a86802c5ec7c5bc3e1e1f100f71232f5c
yuanjiang-lance <lance682@qq.com>
2023-09-01 17:25:05 -0500

    Newly submit: ACMD, ACMD_adapt and their examples

--- TFD/ACMD/Example1_ACMD.m
+++ TFD/ACMD/Example1_ACMD.m
@@ -32,8 +32,42 @@ iniIF1 = f1peak * ones(1, length(Sig));
 
 [IFest1, Sigest1, IAest1] = ACMD(Sig, Fs, iniIF1, tao, mu, tol);
 
-%%
+%% Component 2 Extraction
+SigRes = Sig - Sigest1;
+[~, fftSpecRes] = myfft(SigRes, Fs);
+[~, findex2] = max(fftSpecRes);
+f2peak = f(findex2);
+iniIF2 = f2peak * ones(1, length(Sig));
 
+[IFest2, Sigest2, IAest2] = ACMD(SigRes, Fs, iniIF2, tao, mu, tol);
+
+%% Estimated IF
+figure(101), hold on, box on;
+set(gcf, 'Color', 'w');
+plot(t, [IF1; IF2], 'b', 'linewidth', 2);   % true IFs
+plot(t, [IFest1; IFest2], 'r--', 'linewidth', 2);   % estimated IF
+set(gcf, 'Position', [20 100 415 365]);
+xlabel('Time (s)', 'FontSize', 14, 'FontName', 'Times New Roman');
+ylabel('Frequency (Hz)', 'FontSize', 14, 'FontName', 'Times New Roman');
+axis([0 T 100 Fs/2]);
+set(gca, 'Fontsize', 14, 'linewidth', 1, 'fontname', 'Times New Roman');
+
+%% Reconstructed Modes
+figure(201)
+set(gcf, 'Position', [20 100 500 500], 'Color', 'w');
+subplot(211), hold on, box on;
+plot(t, Sig1, 'k', 'linewidth', 1);
+plot(t, Sigest1, 'b--', 'linewidth', 1);
+xlabel('Time (s)', 'FontSize', 14, 'FontName', 'Times New Roman');
+ylabel('C1', 'FontSize', 14, 'FontName', 'Times New Roman');
+set(gca, 'Fontsize', 14, 'linewidth', 1, 'FontName', 'Times New Roman');
+
+subplot(212), hold on, box on;
+plot(t, Sig2, 'k', 'linewidth', 1);
+plot(t, Sigest2, 'b--', 'linewidth', 1);
+xlabel('Time (s)', 'FontSize', 14, 'FontName', 'Times New Roman');
+ylabel('C2', 'FontSize', 14, 'FontName', 'Times New Roman');
+set(gca, 'Fontsize', 14, 'linewidth', 1, 'FontName', 'Times New Roman');
 
 %% Remove Path
 rmpath(path);
